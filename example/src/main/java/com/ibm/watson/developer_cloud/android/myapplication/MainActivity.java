bbc37acb29754082f4da8318eb305b544dfad8df
Harrison <hgsaylor@us.ibm.com>
2016-04-25 22:58:53 -0500

    Add STT functionality

--- example/src/main/java/com/ibm/watson/developer_cloud/android/myapplication/MainActivity.java
+++ example/src/main/java/com/ibm/watson/developer_cloud/android/myapplication/MainActivity.java
@@ -16,10 +16,15 @@
 
 package com.ibm.watson.developer_cloud.android.myapplication;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -38,9 +43,13 @@ import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.websocket.RecognizeCallback;
 import com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech;
 import com.ibm.watson.developer_cloud.text_to_speech.v1.model.Voice;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+  private final String TAG = "MainActivity";
+
   private RadioGroup targetLanguage;
   private EditText input;
   private ImageButton mic;
@@ -52,6 +61,17 @@ public class MainActivity extends AppCompatActivity {
   private TextToSpeech textService;
   private LanguageTranslation translationService;
   private Language selectedTargetLanguage = Language.SPANISH;
+
+  private AudioTrack audioTrack;
+  private static final int RECORDER_BPP = 16;
+  private static final int RECORDER_SAMPLERATE = 48000;
+  private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_STEREO;
+  private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+  private Thread recordingThread;
+  private int bufferSize;
+
+  private MediaPlayer mPlayer;
+  private int sampleRate;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -101,28 +121,9 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
           @Override public void run() {
             try {
+              //FileInputStream audio = new FileInputStream(getResources().);
               speechService.recognizeUsingWebSocket(new MicrophoneInputStream(),
-                  getRecognizeOptions(), new RecognizeCallback() {
-
-                    @Override public void onTranscription(SpeechResults speechResults){
-                      String text = speechResults.getResults().get(0).getAlternatives().get(0).getTranscript();
-                      showMicText(text);
-                    }
-
-                    @Override public void onConnected() {
-
-                    }
-
-                    @Override public void onError(Exception e) {
-                      showError(e);
-                      enableMicButton();
-                    }
-
-                    @Override public void onDisconnected() {
-                      enableMicButton();
-                    }
-
-                  });
+                  getRecognizeOptions(), new MicrophoneRecognizeDelegate());
             } catch (Exception e) {
               showError(e);
             }
@@ -147,7 +148,7 @@ public class MainActivity extends AppCompatActivity {
       }
     });
 
-    translate.addTextChangedListener(new EmptyTextWatcher() {
+    translatedText.addTextChangedListener(new EmptyTextWatcher() {
       @Override public void onEmpty(boolean empty) {
         if (empty) {
           play.setEnabled(false);
@@ -164,7 +165,7 @@ public class MainActivity extends AppCompatActivity {
         textService.synthesize(translatedText.getText().toString(), Voice.EN_LISA)
             .enqueue(new ServiceCallback<InputStream>() {
               @Override public void onResponse(InputStream stream) {
-                playTranslation(stream);
+                playTranscription(stream);
               }
 
               @Override public void onFailure(Exception e) {
@@ -208,8 +209,26 @@ public class MainActivity extends AppCompatActivity {
     });
   }
 
-  private void playTranslation(InputStream stream) {
-
+  private void playTranscription(InputStream stream) {
+        try {
+          byte[] data = convertStreamToByteArray(stream);
+          if (data.length > 28) {
+            sampleRate = readInt(data, 24);
+          }
+          int headSize = 44, metaDataSize = 48;
+          int destPos = headSize + metaDataSize;
+          int rawLength = data.length - destPos;
+          byte[] d = new byte[rawLength];
+          System.arraycopy(data, destPos, d, 0, rawLength);
+          initPlayer();
+          audioTrack.write(d, 0, d.length);
+          stream.close();
+          if (audioTrack != null && audioTrack.getState() != AudioTrack.STATE_UNINITIALIZED) {
+            audioTrack.release();
+          }
+        } catch (IOException e2) {
+          Log.e(TAG, e2.getMessage());
+        }
   }
 
   private SpeechToText initSpeechToTextService() {
@@ -264,4 +283,61 @@ public class MainActivity extends AppCompatActivity {
 
     public abstract void onEmpty(boolean empty);
   }
+
+  private class MicrophoneRecognizeDelegate implements RecognizeCallback {
+
+    @Override public void onTranscription(SpeechResults speechResults) {
+      String text = speechResults.getResults().get(0).getAlternatives().get(0).getTranscript();
+      showMicText(text);
+    }
+
+    @Override public void onConnected() {
+
+    }
+
+    @Override public void onError(Exception e) {
+      showError(e);
+      enableMicButton();
+    }
+
+    @Override public void onDisconnected() {
+      enableMicButton();
+    }
+  }
+
+  /**
+   * Initialize AudioTrack by getting buffersize
+   */
+  public void initPlayer() {
+    synchronized (this) {
+      int bs = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
+      audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+          sampleRate,
+          AudioFormat.CHANNEL_OUT_MONO,
+          AudioFormat.ENCODING_PCM_16BIT,
+          bs,
+          AudioTrack.MODE_STREAM);
+      if (audioTrack != null)
+        audioTrack.play();
+    }
+  }
+
+  private static byte[] convertStreamToByteArray(InputStream is) throws IOException {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    byte[] buff = new byte[10240];
+    int i;
+    while ((i = is.read(buff, 0, buff.length)) > 0) {
+      baos.write(buff, 0, i);
+    }
+
+    return baos.toByteArray();
+  }
+
+  protected static int readInt(final byte[] data, final int offset)  {
+    return (data[offset] & 0xff) |
+        ((data[offset+1] & 0xff) <<  8) |
+        ((data[offset+2] & 0xff) << 16) |
+        (data[offset+3] << 24); // no 0xff on the last one to keep the sign
+  }
+
 }
