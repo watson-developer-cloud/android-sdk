7874fb02f78c8c9cb8b039777348a303572ca445
Blake Ball <bhball@us.ibm.com>
2016-09-26 15:24:31 -0500

    Tests

---
+++ library/src/test/java/com/ibm/watson/developer_cloud/android/library/CameraHelperTest.java
@@ -0,0 +1,16 @@
+package com.ibm.watson.developer_cloud.android.library;
+
+import org.junit.Test;
+
+
+/**
+ * Created by blakeball on 9/21/16.
+ */
+
+public class CameraHelperTest {
+
+  @Test public void testPictureTakenIntent() {
+    //Intents.init();
+
+  }
+}
