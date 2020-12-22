28676ad2b378186d018990822317519f862989a8
Kevin Kowalski <kevin.kowalski@ibm.com>
2020-12-22 14:25:26 -0600

    test(application): update tests

--- example/src/androidTest/java/com/ibm/watson/developer_cloud/android/myapplication/ApplicationTest.java
+++ example/src/androidTest/java/com/ibm/watson/developer_cloud/android/myapplication/ApplicationTest.java
@@ -17,13 +17,11 @@
 package com.ibm.watson.developer_cloud.android.myapplication;
 
 import android.app.Application;
-import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest {
   public ApplicationTest() {
-    super(Application.class);
   }
 }
