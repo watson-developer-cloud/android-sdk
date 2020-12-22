9b87806bc8691ed06edcf44d78125293a0ea6e45
Kevin Kowalski <kevin.kowalski@ibm.com>
2020-12-22 14:25:26 -0600

    test(application): update tests

--- library/src/androidTest/java/com/ibm/watson/developer_cloud/android/library/ApplicationTest.java
+++ library/src/androidTest/java/com/ibm/watson/developer_cloud/android/library/ApplicationTest.java
@@ -15,18 +15,15 @@
 package com.ibm.watson.developer_cloud.android.library;
 
 import android.app.Application;
-import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest {
   
   /**
    * Instantiates a new application test.
    */
   public ApplicationTest() {
-    super(Application.class);
-
   }
 }
